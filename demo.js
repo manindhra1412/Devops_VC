897f350cd041f464b1171fd9341d393ff00880ec
Pranavstark8 <mathepranav007@gmail.com>
2024-09-06 17:29:00 +0530

    Pranav

--- demo.js
+++ demo.js
@@ -1,5 +1,14 @@
 console.log("Uzumaki Naruto")
 
 let arr = [12,20,40,2000,55];
-
 console.log(arr);
+
+var obj ={
+    age: 20,
+    name: naruto,
+    clan: uzumaki,
+    chakra_type: wind
+}
+
+console.log("New Branch Line");
+console.log(obj)
